2f393c9d6babd7cd96ff1091385d12669ec4c86c
andro951 <andro951@gmail.com>
2023-09-27 15:05:48 -0400

    Add project files.

---
+++ File Renamer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+class Program {
+	static void Main(string[] args) {
+		// Specify the folder path where the files are located
+		string rootFolderPath = @"C:\Users\Isaac\Documents\my games\Terraria\tModLoader\ModSources\VacuumBags\Items\Sprites";
+
+		DirectoryInfo rootDirectory = new DirectoryInfo(rootFolderPath);
+		List<FileInfo> files = rootDirectory.GetFiles().ToList();
+		string findString = " - Copy.png";
+		string replaceString = "_Hover.png";
+		foreach (FileInfo file in files) {
+			if (!file.Name.EndsWith(findString))
+				continue;
+
+			File.Move(file.FullName, file.FullName.Replace(findString, replaceString));
+		}
+	}
+}
